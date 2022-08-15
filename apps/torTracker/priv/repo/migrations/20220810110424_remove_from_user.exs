ca3cb2ab2753049f90839e51eb3df52a52adf401
dusanveljkovic <dusanveljkovickv@gmail.com>
2022-08-15 15:32:07 +0200

    fixed migrations

--- apps/torTracker/priv/repo/migrations/20220810110424_remove_from_user.exs
+++
@@ -1,7 +0,0 @@
-defmodule TorTracker.Repo.Migrations.RemoveFromUser do
-  use Ecto.Migration
-
-  def change do
-
-  end
-end
--- apps/torTracker/priv/repo/migrations/20220810111727_create_info.exs
+++ apps/torTracker/priv/repo/migrations/20220810111727_create_info.exs
@@ -3,14 +3,13 @@ defmodule TorTracker.Repo.Migrations.CreateInfo do
 
   def change do
     create table(:info) do
-      add :cpu_usage, :float
-      add :ram_usage, :integer
-      add :uptime_sec, :integer
       add :fingerprint, :string
       add :name, :string
       add :bandwidth_limit, :float
       add :bandwidth_burst, :float
       add :flags, {:array, :string}
+      add :ip, :string
+      add :port, :integer
       add :user_id, references(:users, on_delete: :nothing)
 
       timestamps()
