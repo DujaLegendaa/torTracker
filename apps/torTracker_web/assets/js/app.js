9b037dc2d288b956b00d469348c898e426c6ef5f
dusanveljkovic <dusanveljkovickv@gmail.com>
2022-08-16 11:58:35 +0200

    added alpinejs

--- apps/torTracker_web/assets/js/app.js
+++ apps/torTracker_web/assets/js/app.js
@@ -24,9 +24,23 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Alpine from "alpinejs"
 
+window.Alpine = Alpine
+Alpine.start()
+let hooks = {}
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken}, 
+  hooks: hooks,
+  dom: {
+    onBeforeElUpdated(from, to) {
+      if (from._x_dataStack) {
+        window.Alpine.clone(from, to);
+      }
+    },
+  },
+});
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
