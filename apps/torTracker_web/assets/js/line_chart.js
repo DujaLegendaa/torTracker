54363d44f36d631100992fac923be12757b91560
dusanveljkovic <dusanveljkovickv@gmail.com>
2022-08-18 16:05:52 +0200

    added relay_info details in live view single

---
+++ apps/torTracker_web/assets/js/line_chart.js
@@ -0,0 +1,60 @@
+import Chart from "chart.js/auto"
+import 'chartjs-adapter-luxon'
+import ChartStreaming from "chartjs-plugin-streaming"
+Chart.register(ChartStreaming)
+
+export default class {
+  constructor(ctx) {
+    const config = {
+      type: "line",
+      data: {
+        datasets: [
+          {
+            label: "Read",
+            data: [],
+            borderColor: "#5a95eb"
+          },
+          {
+            label: "Write",
+            data: [],
+            borderColor: "#8deb98"
+          }
+        ]
+      },
+      options: {
+        datasets: {
+          line: {
+            tension: 0.3
+          }
+        },
+        plugins: {
+          streaming: {
+            duration: 60 * 1000,
+            delay: 1500
+          }
+        },
+        scales: {
+          x: {
+            type: "realtime"
+          },
+          y: {
+
+          }
+        }
+      }
+    }
+
+    this.chart = new Chart(ctx, config)
+  }
+
+  addPoint(label, value) {
+    const dataset = this._findDataset(label)
+    dataset.data.push({x: Date.now(), y: value})
+    this.chart.update()
+  }
+
+  _findDataset(label) {
+    return this.chart.data.datasets.find((dataset) => dataset.label == label)
+  }
+
+}
--- apps/torTracker_web/assets/js/relay_info_charts.js
+++ apps/torTracker_web/assets/js/relay_info_charts.js
@@ -2,39 +2,34 @@ import Chart from "chart.js/auto"
 export default function make_chart(element) {
   const ctx = element.getContext('2d')
   const chart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      datasets: [
+        {
+          label: 'Read',
+          data: [100, 200, 300, 100, 200, 300, 100, 200, 300, 400],
+          borderColor: '#5a95eb',
+        },
+        {
+          label: 'Write',
+          data: [300, 200, 100, 300, 200, 100, 300, 200, 100, 400],
+          borderColor: '#8deb98',
+        },
+      ],
     },
     options: {
       animation: false,
       responsive: false,
       maintainAspectRatio: false,
         scales: {
-            y: {
-                beginAtZero: true
+          xAxis: {
+            //type: "time",
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit: 5
             }
+          }
         }
     }
   })
